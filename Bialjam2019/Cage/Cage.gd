e078be389f66d36ec6ae3095a0f9e339e2f43978
Kiel <kkielek97@gmail.com>
2019-06-08 23:41:47 +0200

    Balanced level

--- Bialjam2019/Cage/Cage.gd
+++ Bialjam2019/Cage/Cage.gd
@@ -6,8 +6,8 @@ enum cage_states {PRISONER, OPENED, CLOSED}
 export (cage_states) var state = cage_states.OPENED setget set_cage_state
 
 var sprite_state = {cage_states.CLOSED: Vector2(0,0),
-					cage_states.OPENED: Vector2(48, 0),
-					cage_states.PRISONER: Vector2(0, 48)}
+					cage_states.OPENED: Vector2(32, 0),
+					cage_states.PRISONER: Vector2(0, 32)}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,7 +21,7 @@ func set_cage_state(new_state) -> void:
 	state = new_state
 	
 	var coords = sprite_state[new_state]
-	$Sprite.region_rect = Rect2(coords.x, coords.y, 48, 48)
+	$Sprite.region_rect = Rect2(coords.x, coords.y, 32, 32)
 
 func _on_Cage_body_entered(body: PhysicsBody2D) -> void:
 	if body == null or state == cage_states.OPENED:
