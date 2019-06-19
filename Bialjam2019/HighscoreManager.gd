d58bf654329601898938043a83c01846d3c05ade
Kiel <kkielek97@gmail.com>
2019-06-19 23:34:53 +0200

    Moved loading scores code to singleton, renamed BestTimes to Highscores

--- Bialjam2019/HighscoreManager.gd
+++ Bialjam2019/HighscoreManager.gd
@@ -1,4 +1,19 @@
 extends Node
 
+var highscores_path := "user://highscore.txt"
+
 func _ready() -> void:
 	print("Hello from HighScore Manager")
+	
+func open_highscores_file() -> void:
+	var f : File = File.new()
+	if not f.file_exists(highscores_path):
+		f.open(highscores_path, File.WRITE)
+		var content : String = ""
+		f.store_string(content)
+		f.close()
+		
+	f.open(highscores_path, File.READ)
+	var content : String = f.get_as_text()
+	print(content)
+	f.close()
---
+++ Bialjam2019/Menu/HighscoresScreen.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+onready var scoreboard_label = $PanelContainer/MarginContainer/VBoxContainer/Scoreboard
+
+func _ready() -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		go_to_main_menu()
+
+func _on_Button_pressed() -> void:
+	$SelectSound.play()
+	yield($SelectSound, "finished")
+	go_to_main_menu()
+
+func go_to_main_menu() -> void:
+	get_tree().change_scene("res://Menu/MainMenu.tscn")
