0c655efd37eefbdf26a2f9709741fae031fcf180
Kiel <kkielek97@gmail.com>
2019-06-20 00:44:19 +0200

    Added displaying highscores

--- Bialjam2019/Menu/HighscoresScreen.gd
+++ Bialjam2019/Menu/HighscoresScreen.gd
@@ -1,9 +1,25 @@
 extends CanvasLayer
 
-onready var scoreboard_label = $PanelContainer/MarginContainer/VBoxContainer/Scoreboard
+onready var scoreboard_label : Label = $PanelContainer/MarginContainer/VBoxContainer/Scoreboard
+
+const SCORE_STRING : String = "%d. %s - %d\n"
 
 func _ready() -> void:
-	pass
+	display_highscores()
+
+func display_highscores() -> void:
+	var scores : Array = HighscoreManager.load_highscores_from_file()
+	var to_display : String
+	
+	if len(scores) == 0:
+		to_display = "This game hasn't been beaten yet. Be first!"
+	else:
+		var i = 0
+		for score in scores:
+			i += 1
+			to_display += SCORE_STRING % [i, score[0], score[1]]
+
+	scoreboard_label.text = to_display.strip_edges()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
