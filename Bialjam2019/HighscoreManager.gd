baa06db1c709dc44d0a922fdf70dc872a2b92549
Kiel <kkielek97@gmail.com>
2019-06-20 00:03:03 +0200

    Added loading scores from file

--- Bialjam2019/HighscoreManager.gd
+++ Bialjam2019/HighscoreManager.gd
@@ -1,19 +1,29 @@
 extends Node
 
-var highscores_path := "user://highscore.txt"
+const NAME_IDX = 0
+const SCORE_IDX = 1
+
+var highscores_path := "user://highscores.txt"
+var highscores_list := []
 
 func _ready() -> void:
-	print("Hello from HighScore Manager")
+	highscores_list = load_highscores_from_file()
+
+func load_highscores_from_file() -> Array:
+	var scores = []
 	
-func open_highscores_file() -> void:
 	var f : File = File.new()
+	
 	if not f.file_exists(highscores_path):
 		f.open(highscores_path, File.WRITE)
-		var content : String = ""
-		f.store_string(content)
 		f.close()
-		
+	
 	f.open(highscores_path, File.READ)
-	var content : String = f.get_as_text()
-	print(content)
+	var content : String = f.get_as_text().strip_edges()
+	if content != "":
+		for record in content.split("\n"):
+			scores.append(record.split(" "))
+			
 	f.close()
+	
+	return scores
