4b062e1ef621ec2d55e9808a8a6c26055c91f925
Kiel <kkielek97@gmail.com>
2019-06-20 11:59:02 +0200

    Added displaying achieved score in another label

--- Bialjam2019/Menu/LevelSummary.gd
+++ Bialjam2019/Menu/LevelSummary.gd
@@ -1,12 +1,14 @@
 extends CanvasLayer
 
+const YOUR_SCORE_MESSAGE : String = "Your score: %d"
 const IN_TOP_MESSAGE : String = "Congratulations! Your score is number %d!"
-const OUT_TOP_MESSAGE : String = "Sorry! Your score is out of best times."
+const OUT_TOP_MESSAGE : String = "Sorry! Your score is below top scores."
 
+onready var comment_label : Label = $PanelContainer/MarginContainer/VBoxContainer/Comment
 onready var score_label : Label = $PanelContainer/MarginContainer/VBoxContainer/Score
 
 func _ready() -> void:
-	update_score_label()
+	update_summary()
 
 func _on_Button_pressed() -> void:
 	$SelectSound.play()
@@ -17,12 +19,14 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		go_to_main_menu()
 
-func update_score_label() -> void:
+func update_summary() -> void:
+	score_label.text = YOUR_SCORE_MESSAGE % [HighscoreManager.new_score]
+	
 	var achieved_rank = HighscoreManager.insert_new_highscore()
 	if achieved_rank < HighscoreManager.SCORES_IN_TOP:
-		score_label.text = IN_TOP_MESSAGE % [achieved_rank + 1]
+		comment_label.text = IN_TOP_MESSAGE % [achieved_rank + 1]
 	else:
-		score_label.text = OUT_TOP_MESSAGE
+		comment_label.text = OUT_TOP_MESSAGE
 
 func go_to_main_menu() -> void:
 	get_tree().change_scene("res://Menu/MainMenu.tscn")
