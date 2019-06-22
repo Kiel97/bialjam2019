80b8b63cabc1d8e0d9b55aeb2d65c32ec8ea92cb
Kiel <kkielek97@gmail.com>
2019-06-23 01:26:26 +0200

    Reordered methods and moved enter key handling to input method

--- Bialjam2019/Menu/LevelSummary.gd
+++ Bialjam2019/Menu/LevelSummary.gd
@@ -18,19 +18,12 @@ func _ready() -> void:
 	textedit.grab_focus()
 	update_summary()
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("enter"):
-		if not save_score_button.disabled and insert_name_cont.visible:
-			_on_SaveScoreButton_pressed()
-
-func _on_Button_pressed() -> void:
-	$SelectSound.play()
-	yield($SelectSound, "finished")
-	go_to_main_menu()
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		go_to_main_menu()
+	if event.is_action_pressed("enter"):
+		if not save_score_button.disabled and insert_name_cont.visible:
+			_on_SaveScoreButton_pressed()
 
 func update_summary() -> void:
 	var score = HighscoreManager.new_score
@@ -41,19 +34,27 @@ func update_summary() -> void:
 	if achieved_rank < HighscoreManager.SCORES_IN_TOP:
 		comment_label.text = IN_TOP_MESSAGE % [achieved_rank + 1]
 		
-		_toggle_player_name_popup(true)
+		toggle_player_name_popup(true)
 		
 		yield(self, "saved_name")
-		
 		HighscoreManager.insert_new_highscore(score, achieved_rank, textedit.text)
 		
 	else:
-		_toggle_player_name_popup(false)
+		toggle_player_name_popup(false)
 		
 		comment_label.text = OUT_TOP_MESSAGE
 
+func toggle_player_name_popup(new_state : bool) -> void:
+	insert_name_cont.visible = new_state
+	main_menu_button.disabled = new_state
+
 func go_to_main_menu() -> void:
 	get_tree().change_scene("res://Menu/MainMenu.tscn")
+
+func _on_Button_pressed() -> void:
+	$SelectSound.play()
+	yield($SelectSound, "finished")
+	go_to_main_menu()
 
 func _on_TextEdit_text_changed(new_text: String) -> void:
 	save_score_button.disabled = true if len(new_text) == 0 else false
@@ -64,8 +65,4 @@ func _on_SaveScoreButton_pressed() -> void:
 	emit_signal("saved_name")
 	yield(click_sound, "finished")
 	
-	_toggle_player_name_popup(false)
-
-func _toggle_player_name_popup(new_state : bool) -> void:
-	insert_name_cont.visible = new_state
-	main_menu_button.disabled = new_state
+	toggle_player_name_popup(false)
