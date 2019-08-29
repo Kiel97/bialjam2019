87a09cbb209f08fd947cddce5073fff36ec82343
Kiel <kkielek97@gmail.com>
2019-08-29 21:08:06 +0200

    Inserted project author and version in code

--- Bialjam2019/Menu/MainMenu.gd
+++ Bialjam2019/Menu/MainMenu.gd
@@ -1,6 +1,15 @@
 extends CanvasLayer
 
+const PROJECT_AUTHOR : String = "Kiel97"
+const PROJECT_VERSION : String = "1.2-dev"
+const PROJECT_INFO : String = "v. %s by %s"
+
+onready var project_label : Label = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/ProjectLabel
+
 var err : int
+
+func _ready() -> void:
+	project_label.text = PROJECT_INFO % [PROJECT_VERSION, PROJECT_AUTHOR]
 
 func _on_StartButton_pressed() -> void:
 	$SelectSound.play()
